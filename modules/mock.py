a969f8404347f7915c59b267fc04d7b98253e23b
jackcasey067 <jackcasey067@gmail.com>
2022-07-27 17:47:18 -0500

    Mock functionality is complete

--- modules/mock.py
+++ modules/mock.py
@@ -5,12 +5,35 @@ from client import Module, ModularClient
 
 
 class Mock(Module):
+    def __init__(self):
+        self._last_msg_in_channels = {}
+
     def activate(self, client: ModularClient):
         client.add_message_handler(self.handle_message)
+        client.add_command_handler(self.handle_command)
 
     async def handle_message(self, client: ModularClient, message: discord.Message):
-        pass # STUB
+        self._last_msg_in_channels[message.channel] = message.content
 
-    
+    async def handle_command(self, client: ModularClient, message: discord.Message):
+        if (message.content.replace(" ", "") == '!mock'):
+            print("Mock command triggered!")
+            if message.channel in self._last_msg_in_channels.keys():
+                await client.send_message(
+                    message.channel, 
+                    Mock.mock_string(self._last_msg_in_channels[message.channel])
+                )
+            else:
+                print("Could not find message to mock.")
 
-last_msg_dict = {}
+
+    def mock_string(s: str):
+        output = ''
+        upper = False
+        for c in s.lower():
+            if upper:
+                output += c.upper()
+            else:
+                output += c
+            upper = not upper
+        return output
