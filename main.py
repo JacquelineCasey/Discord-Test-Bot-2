f33f133a1305820e0a726d874ef29568172f12c2
jackcasey067 <jackcasey067@gmail.com>
2022-07-27 14:48:55 -0500

    Secrets are safe, ready for bot to deploy on Heroku

---
+++ main.py
@@ -0,0 +1,30 @@
+
+import discord
+from discord.message import Message
+from discord.channel import TextChannel
+from dotenv import load_dotenv
+import os
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message: Message):
+        print(f'Message from {message.author}: {message.content}')
+        await self.process_message(message)
+
+    async def process_message(self, msg: Message):
+        if msg.content[0] == '!':
+            await self.send_message(msg.channel, 'I saw that!')
+
+    async def send_message(self, channel: TextChannel, msg):
+        print(f'Sending message "{msg}" to channel "{channel}"')
+        await channel.send(msg)
+
+
+load_dotenv() # Does nothing if it can't find .env
+DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+
+client = MyClient()
+client.run(DISCORD_BOT_TOKEN)
