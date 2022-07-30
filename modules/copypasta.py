58929dc0275513297d306474daea82cc6cb2d2c7
jackcasey067 <jackcasey067@gmail.com>
2022-07-30 16:07:18 -0500

    Minimize amount of code under with block

--- modules/copypasta.py
+++ modules/copypasta.py
@@ -9,12 +9,12 @@ from client import Module, ModularClient
 class CopyPasta(Module):
     def __init__(self, file_path):
         self.triggers_to_pastas = {}
-
         with io.open(file_path) as file:
             data: dict = json.load(file)
-            for key in data.keys():
-                for trigger in data[key]["triggers"]:
-                    self.triggers_to_pastas[trigger] = data[key]["text"]
+            
+        for key in data.keys():
+            for trigger in data[key]["triggers"]:
+                self.triggers_to_pastas[trigger] = data[key]["text"]
 
     @Module.message_handler
     async def recite_pasta(self, client: ModularClient, message: discord.Message):
